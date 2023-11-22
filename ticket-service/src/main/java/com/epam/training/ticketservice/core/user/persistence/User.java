9c68a2cd512dc76a5449517fdc5dc110b125c180
Somogyi Patrik <patreszkaa@gmail.com>
2023-11-22 22:47:08 +0100

    User létrehozva

---
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/user/persistence/User.java
@@ -0,0 +1,31 @@
+package com.epam.training.ticketservice.core.user.persistence;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    public User(String username, String password, UserRole role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+}
---
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/user/persistence/UserRole.java
@@ -0,0 +1,6 @@
+package com.epam.training.ticketservice.core.user.persistence;
+
+public enum UserRole {
+    ADMIN,
+    USER
+}
